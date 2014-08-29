66e3d949c6c5c78e6c7f139db17f087f133fe5c8
christophholtmann <christoph.holtmann@live.de>
2014-08-29 19:59:13 +0200

    Add Folder again First Movement Prototype: -Move to mouse position

---
+++ BattelBoard/Assets/Scripts/Controller Scripts/MoveToMousePosition.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToMousePosition : MonoBehaviour
+{
+    // Serializeable variables
+    [SerializeField]
+    Transform mousePositionTarget = null;
+
+    // Navmesh variables
+    NavMeshAgent navMeshAgent;
+
+    void Awake()
+    {
+        navMeshAgent = gameObject.AddComponent<NavMeshAgent>();
+        navMeshAgent.baseOffset = 0.3f;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mousePositionTarget)
+        {
+            // Set destination
+            navMeshAgent.SetDestination(mousePositionTarget.position);
+        }
+    }
+}
---
+++ BattelBoard/Assets/Scripts/Input Scripts/MouseClickPosition.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseClickPosition : MonoBehaviour
+{
+    [SerializeField]
+    Transform mousePositionTarget = null;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit, 1 << 8))
+            {
+                mousePositionTarget.position = hit.point;
+            }
+        }
+    }
+}
