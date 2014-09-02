8e4594c453da7ec2495389f6a0891d82100478b0
ChristophHoltmann <christoph.holtmann@live.de>
2014-09-02 17:35:20 +0200

    Prefabs and Materials

---
+++ BattelBoard/Assets/Scripts/Controller Scripts/SelectObject.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectObject : MonoBehaviour
+{
+    // Movement variables
+    MoveToMousePosition moveToMousePosition;
+
+    void Awake()
+    {
+        moveToMousePosition = GetComponent<MoveToMousePosition>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (transform.collider.Raycast(ray, out hit, 100))
+            {
+                moveToMousePosition.Selected = true;
+            }
+            else if(moveToMousePosition.Selected)
+            {
+                moveToMousePosition.Selected = false;
+            }
+        }
+    }
+}
---
+++ BattelBoard/Assets/Scripts/GUI Scripts/ShowDistanceToTaget.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowDistanceToTaget : MonoBehaviour
+{
+    // Textmesh variables
+    TextMesh textMesh;
+    MoveToMousePosition moveToMousePosition;
+
+    void Awake()
+    {
+        textMesh = GetComponent<TextMesh>();
+        moveToMousePosition = GetComponentInParent<MoveToMousePosition>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(textMesh && moveToMousePosition.enabled)
+        {
+            int distance = (int)moveToMousePosition.GetDistanceToTarget();
+            textMesh.text = distance.ToString();
+
+            // Look to camera
+            transform.LookAt(Camera.main.transform);
+            transform.Rotate(new Vector3(0, 180, 0));
+        }
+    }
+}
